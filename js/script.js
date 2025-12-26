d22118b57b1b6062d12e52c7e85d0b66232faac0
talhatabani <30605303+talhatabani@users.noreply.github.com>
2025-12-26 06:33:51 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+AOS.init({
+  duration: 900,
+  once: true,
+  easing: 'ease-in-out'
+});
+
+GLightbox({
+  touchNavigation: true,
+  loop: true,
+  zoomable: true
+});
+
+AOS.init({duration:900,once:true});
+GLightbox({loop:true,zoomable:true});
